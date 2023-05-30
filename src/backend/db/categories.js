f95f06beb9e76d227618e71c143bb1ee53c4ccc9
Prateek Suman <prateeksuman51@gmail.com>
2023-05-30 15:12:47 +0530

    First Commit

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,20 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Men",
+    url: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/2215238/2018/1/18/11516279440759-Roadster-Men-Grey-Melange-Striped-Round-Neck-T-shirt-2671516279440577-1.jpg",
+    description: "Men's Clothing - Buy Clothing for Men Online in India",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "Women",
+    url: "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/21126868/2023/3/28/49f84428-77d1-4795-bf59-81804c3f9bf11679999126159-Anouk-Women-Kurta-Sets-7381679999125464-1.jpg",
+    description: "Women's Clothing - Buy Clothing for Women Online in India",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "Kids",
+    url: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/22257502/2023/3/8/a1be671d-ae84-4c53-93cc-f2542c3e6b271678274363991Shirts1.jpg",
+    description: "Kid's Clothing - Buy Clothing for Kids Online in India",
   },
 ];
